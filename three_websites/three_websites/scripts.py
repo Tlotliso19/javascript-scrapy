7fd665d5d4ee5b5445608cc1e3089fd55d8b25a8
Tlotliso19 <160712460+Tlotliso19@users.noreply.github.com>
2025-02-10 09:13:42 +0000

    DELETED

--- three_websites/three_websites/scripts.py
+++ three_websites/three_websites/scripts.py
@@ -6,5 +6,6 @@ my_secret = os.getenv('DATABASE_URL')
 # Use the secret (e.g., print or make an API call)
 print(f"My secret is: {my_secret}")
 
-#
+API_KEY='xau_L9zPEHvPkEIbsOJxRSZN8BHchfAgDq640'
+DATABASE_URL='postgresql://7ni00r:xau_L9zPEHvPkEIbsOJxRSZN8BHchfAgDq640@us-east-1.sql.xata.sh/home:main?sslmode=require'
 print("$$$$$$")
